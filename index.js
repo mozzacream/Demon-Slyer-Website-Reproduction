bae558dfb1c7c0e97ffcc81941948b1b54193162
ryoki <mozza.feb.cream@gmail.com>
2023-03-20 23:14:12 +0900

    commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 "use strict";
+const dropMenuMark = document.getElementsByClassName('material-symbols-outlined')[0];
+console.log(dropMenuMark);
+dropMenuMark.addEventListener('click', () => {
+    alert('ok')
+})
