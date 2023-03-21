e3955bae23bcfb1f051ff96e49c6cf68aac9cdc7
ryoki <mozza.feb.cream@gmail.com>
2023-03-21 09:30:31 +0900

    commit

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 "use strict";
 const dropMenuMark = document.getElementsByClassName('material-symbols-outlined')[0];
-console.log(dropMenuMark);
+const dropMenu = document.getElementById('drop-menu');
+const menuBack = document.getElementById('menu-back');
+const subBlock = document.getElementById('sub-katanakaji');
+
+
 dropMenuMark.addEventListener('click', () => {
-    alert('ok')
+    dropMenu.classList.toggle('menu-open');
+    menuBack.classList.toggle('visible');
+    dropMenuMark.classList.toggle('rotate');
 })
+
+const playCover = document.getElementsByClassName('cover')[0];
+playCover.addEventListener('mouseover', () => {
+    playCover.classList.add('visible');
+})
+subBlock.addEventListener('mouseleave', () => {
+    playCover.classList.remove('visible');
+})
+dropMenuMark.get
